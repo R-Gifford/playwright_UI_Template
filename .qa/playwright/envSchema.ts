a615f17aeb784e1c0c875d5745d06ac65d7d4023
Robert Gifford <rgifford@conservice.com>
2024-07-16 13:19:09 -0600

    Update Variables

--- .qa/playwright/envSchema.ts
+++ .qa/playwright/envSchema.ts
@@ -1,9 +1,10 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const envSchema = z.object({
-    TEST_USER: z.string(),
-    SECRET_PASSWORD: z.string(),
-    BASE_URL: z.string(),
-    AUTH_URL: z.string(),
-    TOKEN: z.string()
+  TEST_USERNAME: z.string(),
+  TEST_PASSWORD: z.string(),
+  BASE_URL: z.string(),
+  AUTH_URL: z.string(),
+  CLIENT_ID: z.string(),
+  TOKEN: z.string(),
 });
